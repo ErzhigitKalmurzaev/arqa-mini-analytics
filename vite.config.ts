7bde74a3b675feb1517942e955e5ecd16b060dcf
Erzhigit <96490425+ErzhigitKalmurzaev@users.noreply.github.com>
2025-10-06 16:42:57 +0600

    FIX

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: false,
-    allowedHosts: [
-      'noninterpolative-sincere-velva.ngrok-free.dev',
-      'localhost',
-    ],
-  },
+  plugins: [react()],
 })
