38e58132f92b7bed3701ee2bf6e34f95db963eb6
Erzhigit <96490425+ErzhigitKalmurzaev@users.noreply.github.com>
2025-10-08 18:38:39 +0600

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     allowedHosts: [
       'noninterpolative-sincere-velva.ngrok-free.dev',
